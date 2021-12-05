cd58ac9dfb480affc7297d7d814d02b6c2a14e4c
anamika-das <95377413+anamika-das@users.noreply.github.com>
2021-12-05 17:17:44 +0530

    Project Commit

---
+++ admin/foot.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<div id="footer">
+	<div class="shell">
+		<span class="left">&copy; <?php echo date("Y");?> - Book My Car</span>
+		<span class="right">
+			Design by Sikha and Anamika</a>
+		</span>
+	</div>
+</div>
+<!-- End Footer -->
---
+++ foot.php
@@ -0,0 +1,49 @@
+<footer>
+  <div class="wrapper footer">
+    <ul>
+      <li class="links">
+        <ul>
+          <li>OUR COMPANY</li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Policy</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </li>
+
+      <li class="links">
+        <ul>
+          <li>OTHERS</li>
+          <li><a href="#">...</a></li>
+          <li><a href="#">...</a></li>
+          <li><a href="#">...</a></li>
+          <li><a href="#">...</a></li>
+        </ul>
+      </li>
+
+      <li class="links">
+        <ul>
+          <li>OUR CAR TYPES</li>
+          <li><a href="#">Wagnor</a></li>
+          <li><a href="#">Santro</a></li>
+          <li><a href="#">Maruti 800</a></li>
+          <li><a href="#">Nano.</a></li>
+        </ul>
+      </li>
+
+      <li class="about">
+        <p>Here you can book car for short trip on rent. Start up and enjoy/p>
+        <ul>
+          <li><a href="" class="facebook" target="_blank"></a></li>
+          <li><a href="" class="twitter" target="_blank"></a></li>
+          <li><a href=" class="google" target="_blank"></a></li>
+          <li><a href="#" class="skype"></a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+
+  <div class="copyrights wrapper">
+    Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by Sikha & Anamika.
+  </div>
+</footer><!--  end footer  -->
